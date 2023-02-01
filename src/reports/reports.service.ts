43774480f99fef08d1743b565540d2e560dfe4d7
Moshood1426 <moshood.yemi10@gmail.com>
2023-02-01 07:25:41 -0500

    reports dto created

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { Reports } from './reports.entity';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(
+    @InjectRepository(Reports)
+    private readonly reportsRepo: Repository<Reports>,
+  ) {}
+
+  create(body: CreateReportDto) {
+    
+  }
+}
